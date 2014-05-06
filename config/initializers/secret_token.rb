6e53c22a3ea5758e9755036da9e62eb93f4a6b4f
bhanu22776 <bhanuprasadmende@gmail.com>
2014-05-06 16:13:20 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mahalaxmi::Application.config.secret_token = 'b1c83c8799d44ceb4de03babf20dad57f23abe09482607fd540a57698902de0388e2f8e5956e19e6d0e0bef8c4d5b907abbb057d9cdca7a21f48a01da07a9ab1'
